fd1fe09fbdf1e6f0a20d8872cbf1b3c899b1c07b
Justin Laird <justin@xdam.com>
2019-03-25 22:39:32 -0700

    return cfn custom resource success message after being triggered on deployment

--- src/updateMySQL/index.js
+++ src/updateMySQL/index.js
@@ -1,6 +1,8 @@
+const cfnCR = require('cfn-custom-resource');
+
 exports.handler = async message => {
   console.log(message);
   console.log("Mr Magic Lambda");
-
+  await cfnCR.sendSuccess('UpdaateMySQL completed', {}, message);
   return {};
 }
