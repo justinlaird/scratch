45776ae62e02a8e96b6078fa04ce1d2ec7c65b81
Justin Laird <justin@xdam.com>
2019-03-26 07:31:36 -0700

    fix typo

--- src/updateMySQL/index.js
+++ src/updateMySQL/index.js
@@ -3,6 +3,6 @@ const cfnCR = require('cfn-custom-resource');
 exports.handler = async message => {
   console.log(message);
   console.log("Mr Magic Lambda");
-  await cfnCR.sendSuccess('UpdaateMySQL completed', {}, message);
+  await cfnCR.sendSuccess('UpdateMySQL completed', {}, message);
   return {};
 }
