a374fe09df2711144c39ede35e18f691c69440f9
Justin Laird <justin@xdam.com>
2019-03-27 13:55:24 -0700

    log DB params

--- src/updateMySQL/index.js
+++ src/updateMySQL/index.js
@@ -1,12 +1,16 @@
 const cfnCR = require('cfn-custom-resource');
 const jsonApiStore = require('xdam-clique-api/services/data-store');
 const jsonApiService = require('xdam-clique-api//services/json-api');
+const config = require('config');
+
 
 
 exports.handler = async message => {
   console.log(message);
   try {
     console.log("Mr Magic Lambda");
+    console.log(`DB HOST ${config.get('database.host')}`);
+    console.log(`DB PORT ${config.get('database.port')}`);
 
    // jsonApiService.initialize(null, true);
    // jsonApiStore.populateDatabase();
@@ -18,3 +22,6 @@ exports.handler = async message => {
   }
   return {};
 }
+
+
+//exports.handler("THE message is this!");
