cec23165e082c43fab265e4509a6e6d39b3eda84
Justin Laird <justin@xdam.com>
2019-03-27 13:26:04 -0700

    import xdam-clique-api services

--- src/updateMySQL/index.js
+++ src/updateMySQL/index.js
@@ -1,8 +1,22 @@
 const cfnCR = require('cfn-custom-resource');
+const jsonApiStore = require('xdam-clique-api/services/data-store');
+const jsonApiService = require('xdam-clique-api//services/json-api');
+
 
 exports.handler = async message => {
   console.log(message);
-  console.log("Mr Magic Lambda");
-  await cfnCR.sendSuccess('UpdateMySQL completed', {}, message);
+  try {
+    console.log("Mr Magic Lambda");
+
+   // jsonApiService.initialize(null, true);
+   // jsonApiStore.populateDatabase();
+  } catch (error) {
+    console.log(error);
+    await cfnCR.sendFailure(error.message, message);
+  } finally {
+    await cfnCR.sendSuccess('UpdateMySQL completed', {}, message);
+  }
   return {};
 }
+
+ exports.handler("THE message is this!");
