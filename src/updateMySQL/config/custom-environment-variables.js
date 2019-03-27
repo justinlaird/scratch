c1fb63146efc89341bc0ddb67643f1393636e71c
Justin Laird <justin@xdam.com>
2019-03-27 14:29:10 -0700

    add config dir

---
+++ src/updateMySQL/config/custom-environment-variables.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "database": {
+    "host": "DB_ADDRESS",
+    "port": "DB_PORT",
+    "databaseName": "DB_NAME",
+    "username": "DB_USER",
+    "password": "DB_PASSWORD",
+  },
+  "server": {
+    "protocol": "SERVER_PROTOCOL",
+    "hostname": "SERVER_HOSTNAME",
+    "address": "SERVER_ADDRESS",
+    "port": "SERVER_PORT",
+    "internalPort": "INTERNAL_SERVER_PORT",
+  },
+  "redis" : {
+    "redisAddress": "REDIS_ADDRESS"
+  },
+  Serverless: {
+    s3: {
+      bucket: "SERVERLESS_S3_BUCKET_NAME",
+      accessKeyId: "SERVERLESS_AWS_ACCESS_KEY_ID",
+      secretAccessKey: "SERVERLESS_AWS_SECRET_ACCESS_KEY",
+      region: "SERVERLESS_AWS_REGION",
+    }
+  },
+}
