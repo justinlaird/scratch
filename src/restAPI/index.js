4b689ada3e6741e029ca4b99a41590c14b9d1a52
Justin Laird <justin@xdam.com>
2019-03-28 13:51:34 -0700

    clean up

--- src/restAPI/index.js
+++ src/restAPI/index.js
@@ -1,22 +1,13 @@
 const serverless = require('serverless-http');
 
-//const jsonApiServer = require('xdam-clique-api/jsonapi-server');
-//exports.handler = serverless(jsonApiServer);
 const {JsonApiController} = require('xdam-clique-api/controllers');
 const express = require('express');
 const bodyParser = require('body-parser');
-
-
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/', JsonApiController);
 module.exports.handler = serverless(app);
-
-
-
-
-
 
 
 //basic hello world with serverless
@@ -33,42 +24,6 @@ app.get('/rest/users', function (req, res) {
 })
 
 module.exports.handler = serverless(app);
-
 */
 
 
-
-/*
-'use strict';
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const config = require('config');
-const {JsonApiController, WelcomeController, TokenController} = require('./controllers');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Headers', req.headers['access-control-request-headers'] || '');
-  if (Array.isArray(config.get('server.accessControlAllow'))) {
-
-    if(config.get('server.accessControlAllow').includes('*')){
-      res.setHeader('Access-Control-Allow-Origin', '*');
-    } else {
-      var origin = req.headers.origin;
-      if(config.get('server.accessControlAllow').includes(origin)){
-        res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-    }
-  }
-  return next();
-});
-
-
-app.use('/welcome', WelcomeController); //sample controller
-app.use(config.get('jwt.tokenRoute'), TokenController);
-app.use('/', JsonApiController);
-
-module.exports = app;
-*/
