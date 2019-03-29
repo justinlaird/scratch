be4aca9d4952e3e869fe29c0eaea9d5a790a10c6
Justin Laird <justin@xdam.com>
2019-03-29 15:46:28 -0700

    add logging

--- src/deployFrontEnd/index.js
+++ src/deployFrontEnd/index.js
@@ -1,5 +1,11 @@
+const cfnCR = require('cfn-custom-resource');
+
+
 exports.handler = async message => {
+  console.log("======MESSAGE=====");
   console.log(message);
+  console.log("======ENDMESSAGE=====");
+
   await cfnCR.sendSuccess('deployFrontEnd', {}, message);
   return {};
 
