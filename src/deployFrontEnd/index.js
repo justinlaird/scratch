97244f5d03f1a48c32bf41bd579563603a645bbf
Justin Laird <justin@xdam.com>
2019-03-29 20:23:47 -0700

    add test code examples

--- src/deployFrontEnd/index.js
+++ src/deployFrontEnd/index.js
@@ -10,3 +10,26 @@ exports.handler = async message => {
   return {};
 
 }
+
+
+//example test invocation
+//sam local generate-event cloudformation create-request | sam local invoke DeployFrontEnd
+
+/*
+exports.handler({
+  "RequestType": "Create",
+  "ResponseURL": "https://cloudformation-custom-resource-response-uswest2.s3-us-west-2.amazonaws.com",
+  "StackId": "arn:aws:cloudformation:us-east-1:123456789012:stack/MyStack/guid",
+  "RequestId": "unique id for this create request",
+  "ResourceType": "Custom::TestResource",
+  "LogicalResourceId": "MyTestResource",
+  "ResourceProperties": {
+    "StackName": "MyStack",
+    "List": [
+      "1",
+      "2",
+      "3"
+    ]
+  }
+});
+*/
