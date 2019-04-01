baa39a77582281b43afee179368381e8503393da
Justin Laird <justin@xdam.com>
2019-04-01 12:59:08 -0700

    change working directory for npx command

--- src/deployFrontEnd/index.js
+++ src/deployFrontEnd/index.js
@@ -49,7 +49,7 @@ exports.handler = async message => {
 
     //await spawnPromise('./node_modules/.bin/ember', ['-v']);
 
-    await spawnPromise('npx', ['ember','build']);
+    await spawnPromise('npx', ['ember','build'], {cwd: tmpDir});
 
     /*
 
